333a2d7db8078a86ab910f3df7acb331ea28b8a8
Bryant K. Feld <bryant.feld@gmail.com>
2025-10-02 02:11:32 +0000

    backend updates

---
+++ backend/src/db/init.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+export function initDatabase() {
+  const DATABASE_URL = 'mongodb://127.0.0.1:27017/blog'
+
+  mongoose.connection.on('open', () => {
+    console.info('successfully connected to database:', DATABASE_URL)
+  })
+  const connection = mongoose.connect(DATABASE_URL)
+  return connection
+}
