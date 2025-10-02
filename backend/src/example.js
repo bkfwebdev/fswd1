095fc0efe4e3cd476693690d484947e53a41075e
Bryant K. Feld <bryant.feld@gmail.com>
2025-10-02 17:31:49 +0000

    post schema added

--- backend/src/example.js
+++ backend/src/example.js
@@ -1,2 +1,16 @@
 import { initDatabase } from './db/init.js';
-initDatabase();
+import { Post } from './db/models/post.js'
+
+await initDatabase();
+
+const post = new Post({
+    title: 'Test Title',
+    author: 'Test Author',
+    contents: 'this is a test post',
+    tags: ['fullstack','webdevelopment'],
+})
+await post.save()
+
+const dbpost = await Post.find()
+console.log(dbpost)
+
