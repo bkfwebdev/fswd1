136e7060929aebfcb574be8105297ae76dbee37e
Bryant K. Feld <bryant.feld@gmail.com>
2025-10-03 22:59:51 +0000

    adding test

--- backend/src/db/init.js
+++ backend/src/db/init.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 
 export function initDatabase() {
-  const DATABASE_URL = 'mongodb://127.0.0.1:27017/blog'
+  const DATABASE_URL = process.env.DATABASE_URL
+  // data base url is 'mongodb://127.0.0.1:27017/blog'
 
   mongoose.connection.on('open', () => {
     console.info('successfully connected to database:', DATABASE_URL)
